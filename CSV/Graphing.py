91179c5e2fe705e672f92e6f54735660bff0d86b
Hwh40 <hwh40@uclive.ac.nz>
2023-01-26 15:18:38 +1300

    Serial

--- CSV/Graphing.py
+++ CSV/Graphing.py
@@ -1,78 +1,105 @@
 # File: Graphing.py
 # Author: Henry Hall
 # Date: 23/01/2022
-# Description: Imports three different sets of data
-# from the serial port. This plots a live graph of this data 
+# Description: Imports different sets of data
+# from the serial port and  plots a live graph
 
 import numpy as np
 import matplotlib.pyplot as plt
 import serial
-import math
 import time
+import keyboard
 
-RUNTIME = 60
-
+#PORT = 'COM7'
+PORT = 'COM3'
+BAUD = 115200
+MAX_OUT = 10
+WIDTH = 20
+EXCEPTIONS = [["b'\\n'", "b'S'"],['\\', '']]
 
 def graph_set(fig, axes):
-    """Graphing settings"""
+    """Sets the graph settings"""
     axes.grid(True)
     axes.set_xlabel('Time (s)')
-    axes.set_title('Sensor')
+    axes.set_title('Readings')
+    
+def plot(fig, axes, xs, ys, i):
+    """Clears graph data and plots new data live"""
+    axes.cla()
+    graph_set(fig, axes)
+    if xs[-1] < WIDTH:
 
+        axes.set_xlim([0, WIDTH])
+    else:
+        axes.set_xlim([xs[-1] - WIDTH, xs[-1]])
+    for j in range(0, i):
+        axes.plot(xs, ys[j], label = f'Reading{j + 1}')
+    axes.legend()
+    plt.pause(0.01)
+
+def set_data(data, xs, ys, start):
+    """Puts time and data into x and y lists"""
+    for i in range(0, len(data)):
+        ys[i].append(data[i])
+    xs.append(time.time() - start)
+    return (xs, ys)
+
+def check_end(axes, xs):
+    """Checks to see if the session has been quit"""
+    if keyboard.is_pressed('Enter'):
+        print('Quit')
+        axes.set_xlim([0, xs[-1]])
+        plt.show()
+        return True
+    else:
+        return False
+
+def set_ys():
+    """Sets an empty y list"""
+    ys = []
+    for i in range(0, MAX_OUT):
+        ys.append([])
+    return ys
+
+def set_start(is_first, start):
+    """sets the initial time"""
+    if is_first == True:
+        start = time.time()
+    return start
+
+get_b = lambda ser: str(ser.read())
 
 def main():
     """The main function"""
-    fig1 = plt.figure("Figure 1")
+    #Set all initial values and empty lists
+    fig1 = plt.figure("Figure")
     axes = plt.axes() 
-    
-    ser = serial.Serial('COM7')
+    ser = serial.Serial(PORT, BAUD)
+    start = time.time()
     print(ser.name)
     is_done = False
-    i = 1
-    j = 0
-    even = 0
-    blank = []
-    empty = []
-    xs = []
-    angle = []
-    st = ''
-    graph_set(fig1, axes)
     is_first = True
-    
+    xs = []
+    ys = set_ys()
+    data = []
+    s = ''
     while is_done != True:
-        byte = (str(ser.read()))
-        if is_first:
-            start = time.time()
-            is_first = False
-        if (time.time() - start) > (RUNTIME):
-            is_done = True
+        #Continuous while loop which reads, proccesses and plots data from the serial port
+        byte = get_b(ser)
         if byte == "b'S'":
-            even = 0
-            byte = str(ser.read())
-            byte = str(ser.read())
-        if byte == "b'\\n'":
-            if even == 0:
-                blank.append(float(st))
-            elif even == 1:
-                empty.append(float(st))
-            elif even == 2:
-                angle.append(float(st))
-                xs.append(time.time() - start)
-                axes.cla()
-                graph_set(fig1, axes)
-                axes.plot(xs, blank, 'r-', label = 'Reading 1')
-                axes.plot(xs, empty, 'b-', label = 'Reading 2')
-                axes.plot(xs, angle, 'g-', label = 'Angle')
-                axes.legend()
-                plt.pause(0.001)
-                j += 1
-            st = ''
-            even += 1
-        elif byte != "b'\\n'" and byte != "b'S'" and list(byte)[2] != '\\':
-            st += list(byte)[-2]
-    print('Finished')
-    plt.show()
-    
-    
+            start = set_start(is_first, start)
+            is_first = False
+            if data != []:
+                set_data(data, xs, ys, start)
+                plot(fig1, axes, xs, ys, len(data))
+            data = []
+            byte = get_b(ser)
+            byte = get_b(ser)
+        if byte == "b'\\n'":  
+            data.append(float(s))
+            s = ''
+        elif byte not in EXCEPTIONS[0] and list(byte)[2] not in EXCEPTIONS[1]:
+            s += list(byte)[-2]
+        is_done = check_end(axes, xs)
 
 main()
